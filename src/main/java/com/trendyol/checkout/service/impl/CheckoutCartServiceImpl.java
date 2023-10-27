57d57c50485330ca38eaa432e15cdf9c31e14723
Altan Şimşir <altansimsir@Altans-MacBook-Air.local>
2023-10-27 23:56:41 +0300

    addItem method refactored.

--- src/main/java/com/trendyol/checkout/service/impl/CheckoutCartServiceImpl.java
+++ src/main/java/com/trendyol/checkout/service/impl/CheckoutCartServiceImpl.java
@@ -3,6 +3,7 @@ package com.trendyol.checkout.service.impl;
 import com.trendyol.checkout.dto.AddItemDTO;
 import com.trendyol.checkout.dto.ResponseDTO;
 import com.trendyol.checkout.entity.*;
+import com.trendyol.checkout.mapper.ItemMapper;
 import com.trendyol.checkout.model.Promotion;
 import com.trendyol.checkout.repository.CheckoutCartRepository;
 import com.trendyol.checkout.repository.ItemRepository;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CheckoutCartServiceImpl implements CheckoutCartService {
+
     private final CheckoutCartRepository checkoutCartRepository;
     private final ItemRepository itemRepository;
 
@@ -22,61 +24,102 @@ public class CheckoutCartServiceImpl implements CheckoutCartService {
     }
 
     public ResponseDTO addItem(AddItemDTO itemDTO) {
-        CheckoutCart checkoutCart = checkoutCartRepository.findById(CheckoutCart.CART_REFERANCE).orElse(new CheckoutCart());
+        CheckoutCart checkoutCart = getCheckoutCart();
+        ResponseDTO validationResult = validateCartConstraints(checkoutCart, itemDTO);
 
-        // Constraints
-        if (checkoutCart.getTotalPrice() + itemDTO.getPrice() * itemDTO.getQuantity() > CheckoutCart.MAX_CART_VALUE) {
-            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_CART_VALUE_ERROR_MESSAGE);
+        if (validationResult != null) {
+            return validationResult;
         }
 
-        if (checkoutCart.getItems().size() >= CheckoutCart.MAX_UNIQUE_ITEM_COUNT) {
-            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_UNIQUE_ITEM_COUNT_ERROR_MESSAGE);
-        }
-
-        if (checkoutCart.getItems().stream().mapToInt(Item::getQuantity).sum() + itemDTO.getQuantity() > CheckoutCart.MAX_QUANTITY_PER_CART) {
-            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_QUANTITY_PER_CART_ERROR_MESSAGE);
-        }
-
-
-        Item item;
-
-        if(itemDTO.getSellerId() == VasItem.VAS_ITEM_SELLER_ID || itemDTO.getCategoryId() == VasItem.VAS_ITEM_CATEGORY_ID){
+        if (isVasItem(itemDTO)) {
             return new ResponseDTO(ResponseDTO.FAILED, VasItem.VAS_ITEM_WRONG_ENDPOINT_MESSAGE);
-        } else if (itemDTO.getCategoryId() == DigitalItem.DIGITAL_ITEM_CATEGORY_ID) {
-            if (checkoutCart.getItems().stream().anyMatch(cartItem -> (cartItem instanceof DefaultItem))) {
+        }
+
+        if (isDigitalItem(itemDTO)) {
+            if (hasDefaultItem(checkoutCart)) {
                 return new ResponseDTO(ResponseDTO.FAILED, DigitalItem.HAS_DEFAULT_ITEM_ERROR_MESSAGE);
             }
-            if (itemDTO.getQuantity() > DigitalItem.DIGITAL_ITEM_QUANTITY_PER_INPUT) {
+            if (isQuantityInvalidForDigitalItem(itemDTO)) {
                 return new ResponseDTO(ResponseDTO.FAILED, DigitalItem.QUANTITY_ERROR_PER_INPUT_MESSAGE);
             }
-            item = new DigitalItem();
         } else {
-            if (checkoutCart.getItems().stream().anyMatch(cartItem -> cartItem instanceof DigitalItem)) {
+            if (hasDigitalItem(checkoutCart)) {
                 return new ResponseDTO(ResponseDTO.FAILED, DefaultItem.HAS_DIGITAL_ITEM_ERROR_MESSAGE);
             }
-            if (itemDTO.getQuantity() > Item.ITEM_QUANTITY_PER_INPUT) {
+            if (isQuantityInvalidForDefaultItem(itemDTO)) {
                 return new ResponseDTO(ResponseDTO.FAILED, Item.QUANTITY_ERROR_PER_INPUT_MESSAGE);
             }
-            item = new DefaultItem();
         }
 
-        item.setItemId(itemDTO.getItemId());
-        item.setCategoryId(itemDTO.getCategoryId());
-        item.setSellerId(itemDTO.getSellerId());
-        item.setPrice(itemDTO.getPrice());
-        item.setQuantity(itemDTO.getQuantity());
+        Item item = ItemMapper.dtoToItem(itemDTO);
+        updateCheckoutCartWithNewItem(checkoutCart, item);
 
+        return new ResponseDTO(ResponseDTO.SUCCESS, CheckoutCart.SUCCESS_MESSAGE);
+    }
+
+    private void updateCheckoutCartWithNewItem(CheckoutCart checkoutCart, Item item) {
         item.setCheckoutCart(checkoutCart);
-
         checkoutCart.getItems().add(item);
+
         Promotion appliedPromotion = Promotion.applyPromotion(checkoutCart);
         checkoutCart.setAppliedPromotionId(appliedPromotion.getPromotionId());
         checkoutCart.setTotalDiscount(appliedPromotion.getDiscount());
-        checkoutCart.setTotalPrice(checkoutCart.getTotalPrice() + item.getPrice() * item.getQuantity() - appliedPromotion.getDiscount());
+
+        double newTotalPrice = checkoutCart.getTotalPrice() + item.getPrice() * item.getQuantity() - appliedPromotion.getDiscount();
+        checkoutCart.setTotalPrice(newTotalPrice);
 
         checkoutCartRepository.save(checkoutCart);
+    }
+    private CheckoutCart getCheckoutCart() {
+        return checkoutCartRepository.findById(CheckoutCart.CART_REFERANCE)
+                .orElse(new CheckoutCart());
+    }
+    private ResponseDTO validateCartConstraints(CheckoutCart checkoutCart, AddItemDTO itemDTO) {
+        if (isCartOverValueLimit(checkoutCart, itemDTO)) {
+            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_CART_VALUE_ERROR_MESSAGE);
+        }
 
-        return new ResponseDTO(ResponseDTO.SUCCESS, CheckoutCart.SUCCESS_MESSAGE);
+        if (isCartAtMaxUniqueItems(checkoutCart)) {
+            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_UNIQUE_ITEM_COUNT_ERROR_MESSAGE);
+        }
+
+        if (isCartOverTotalQuantityLimit(checkoutCart, itemDTO)) {
+            return new ResponseDTO(ResponseDTO.FAILED, CheckoutCart.MAX_QUANTITY_PER_CART_ERROR_MESSAGE);
+        }
+        return null;
+    }
+
+    private boolean isCartOverValueLimit(CheckoutCart cart, AddItemDTO itemDTO) {
+        double newTotal = cart.getTotalPrice() + itemDTO.getPrice() * itemDTO.getQuantity();
+        return newTotal > CheckoutCart.MAX_CART_VALUE;
+    }
+
+    private boolean isCartAtMaxUniqueItems(CheckoutCart cart) {
+        return cart.getItems().size() >= CheckoutCart.MAX_UNIQUE_ITEM_COUNT;
+    }
+
+    private boolean isCartOverTotalQuantityLimit(CheckoutCart cart, AddItemDTO itemDTO) {
+        int totalQuantity = cart.getItems().stream().mapToInt(Item::getQuantity).sum() + itemDTO.getQuantity();
+        return totalQuantity > CheckoutCart.MAX_QUANTITY_PER_CART;
+    }
+
+    private boolean isVasItem(AddItemDTO itemDTO) {
+        return itemDTO.getSellerId() == VasItem.VAS_ITEM_SELLER_ID || itemDTO.getCategoryId() == VasItem.VAS_ITEM_CATEGORY_ID;
+    }
+    private boolean isDigitalItem(AddItemDTO itemDTO) {
+        return itemDTO.getCategoryId() == DigitalItem.DIGITAL_ITEM_CATEGORY_ID;
+    }
+    private boolean hasDefaultItem(CheckoutCart cart) {
+        return cart.getItems().stream().anyMatch(item -> item instanceof DefaultItem);
+    }
+    private boolean isQuantityInvalidForDigitalItem(AddItemDTO itemDTO) {
+        return itemDTO.getQuantity() > DigitalItem.DIGITAL_ITEM_QUANTITY_PER_INPUT;
+    }
+    private boolean hasDigitalItem(CheckoutCart cart) {
+        return cart.getItems().stream().anyMatch(item -> item instanceof DigitalItem);
+    }
+    private boolean isQuantityInvalidForDefaultItem(AddItemDTO itemDTO) {
+        return itemDTO.getQuantity() > Item.ITEM_QUANTITY_PER_INPUT;
     }
 
 }
